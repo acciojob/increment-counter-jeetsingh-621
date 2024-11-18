cedd4729e2a91f026ac2823705691b429e01fea8
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-18 11:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const button = document.getElementById("incrementBtn");
+const counter = document.getElementById("counter");
+
+let count = 0;
+button.addEventListener("click",()=>{
+	alert(`current value : ${count}`);
+	count++;
+	counter.textContent = count;
+})
+
+
+
